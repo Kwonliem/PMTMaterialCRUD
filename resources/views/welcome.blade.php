362c5c89c30cfc6019f811682205dc2b9af684d8
Liem <liemyoesuf@gmail.com>
2025-01-24 14:25:57 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To Student Management</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css')}}">
 </head>
 <body>
-    
+    <h1 class="text-center mt-3">
+        Assalamu Alaikum <br> Welcome To Student Management System
+    </h1>
+    <div class="text-center mt-3">
+        <a href="#" class="btn btn-primary">Manage Student</a>
+    </div>
 </body>
 </html>
